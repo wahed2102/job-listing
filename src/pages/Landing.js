47bab17c206f1bd7f455839b4f5cc43af1962c3e
Wahed Ali <wahed.khan@rediffmail.com>
2023-01-08 00:44:08 +0530

    Landing Page

---
+++ src/pages/Landing.js
@@ -0,0 +1,28 @@
+import logo from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobs listing" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Post a job on the #1 job site*. Job Listing helps the
+            right candidates find you. The best way to get your jobs.
+          </p>
+          <button className="btn btn-hero">Login / Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
